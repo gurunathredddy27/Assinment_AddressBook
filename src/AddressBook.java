0d65579dbe953acc70f245d78778fdeec755884b
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-04-04 13:10:15 +0530

    edit contact

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,27 +6,87 @@ public class AddressBook{
         Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
 
-        System.out.println("Enter new Contact information");
-        System.out.println("First name: ");
-        String firstname = sc.nextLine();
-        System.out.println("Last name: ");
-        String lastname = sc.nextLine();
-        System.out.println("Adrees: ");
-        String address = sc.nextLine();
-        System.out.println("City: ");
-        String city = sc.nextLine();
-        System.out.println("State: ");
-        String state = sc.nextLine();
-        System.out.println("Zip code: ");
-        String zip = sc.nextLine();
-        System.out.println("Phone number: ");
-        String phoneNum = sc.nextLine();
-        System.out.println("email: ");
-        String email = sc.nextLine();
-        System.out.println("Contact added :)");
+        addressBook.addContact(new Contact("Ram", "Krisha", "123 conlony", "HYD", "Telang", "12345", "34565423", "ram27@gmail.com"));
 
-        contact newContact = new contact(firstname , lastname,  //newContact is obj
-                address, city, state, zip , phoneNum, email);
+        while (true) {
+            System.out.println("Enter an option!");
+            System.out.println("1. Enter new Contact information");
+            System.out.println("2. Edit an contact exiting ");
+            System.out.println("3. Exit");
+
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            if(option == 1) {
+                System.out.println("First name: ");
+                String firstname = sc.nextLine();
+                System.out.println("Last name: ");
+                String lastname = sc.nextLine();
+                System.out.println("Adrees: ");
+                String address = sc.nextLine();
+                System.out.println("City: ");
+                String city = sc.nextLine();
+                System.out.println("State: ");
+                String state = sc.nextLine();
+                System.out.println("Zip code: ");
+                String zip = sc.nextLine();
+                System.out.println("Phone number: ");
+                String phoneNum = sc.nextLine();
+                System.out.println("email: ");
+                String email = sc.nextLine();
+                System.out.println("Contact added :)");
+
+                Contact newContact = new Contact(firstname, lastname,  //newContact is obj
+                        address, city, state, zip, phoneNum, email);
+            }else if(option ==2){
+                System.out.println("Enter your First name to edit your contact details");
+                String firstname = sc.nextLine();
+                System.out.println("Enter your Last name to edit your contact details");
+                String lastname = sc.nextLine();
+
+                Contact editContact = addressBook.findContact(firstname,lastname);
+                if(editContact != null){
+                    System.out.println("Enter the new details for the contact:");
+                    System.out.print("First name: ");
+                    editContact.setFname(sc.nextLine());
+                    System.out.print("Last name: ");
+                    editContact.setLname(sc.nextLine());
+                    System.out.print("Address: ");
+                    editContact.setAdress(sc.nextLine());
+                    System.out.print("City: ");
+                    editContact.setCity(sc.nextLine());
+                    System.out.print("State: ");
+                    editContact.setState(sc.nextLine());
+                    System.out.print("Zip code: ");
+                    editContact.setZip(sc.nextLine());
+                    System.out.print("Phone number: ");
+                    editContact.setPhonenum(sc.nextLine());
+                    System.out.print("Email : ");
+                    editContact.setEmail(sc.nextLine());
+
+                    Contact.setFname(editContact.fname);
+                    Contact.setLname(editContact.lname);
+                    Contact.setAddress(editContact.adress);
+                    Contact.setCity(editContact.city);
+                    Contact.setState(editContact.state);
+                    Contact.setZip(editContact.zip);
+                    Contact.setPhonenum(editContact.phonenum);
+                    Contact.setEmail(editContact.email);
+
+                    System.out.println("contact edited successfully");
+                }
+
+            }else  {
+                System.out.println("EXIT");
+            }
+        }
+    }
+
+    private Contact findContact(String firstname, String lastname) {
+        return null;
+    }
+
+    private void addContact(Contact contact) {
     }
 }
 
--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,4 @@
-class contact {
+class Contact {
 
     String fname;
     String lname;
@@ -9,7 +9,7 @@ class contact {
     String phonenum;
     String email;
 
-    public contact(String firstName, String lastName, String adress, String city, String state,
+    public Contact(String firstName, String lastName, String adress, String city, String state,
                    String zipcode, String phoneNum, String email) {
         this.fname = firstName;
         this.lname = lastName;
@@ -22,42 +22,69 @@ class contact {
         System.out.println(firstName);
     }
 
-    public String getFname() {
+    public static void setAddress(String lname) {
+    }
+
+    public  String getFname() {
 
         return fname;
+    }
+    public static void setFname(String fname){
+        fname = fname;
     }
 
     public String getLname() {
 
         return lname;
     }
+    public static void setLname(String lname){
+        lname = lname;
+    }
 
     public String getAdress() {
 
         return adress;
+    }
+    public void setAdress(String adress){
+        adress = adress;
     }
 
     public String getCity() {
 
         return city;
     }
+    public static void setCity(String city){
+        city = city;
+    }
 
     public String getState() {
 
         return state;
     }
+    public static void setState(String state){
+        state = state;
+    }
 
     public String getZip() {
         return zip;
+    }
+    public static void setZip(String zip){
+        zip = zip;
     }
 
     public String getPhonenum() {
 
         return phonenum;
     }
+    public static void setPhonenum(String phonenum){
+        phonenum = phonenum;
+    }
 
     public String getEmail() {
 
         return email;
+    }
+    public static void setEmail(String email){
+        email = email;
     }
 }
